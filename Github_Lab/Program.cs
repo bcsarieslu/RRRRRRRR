ace2ec70bf4bd1e9d590870d23955a8d9aad6aeb
Glenn Chen <glenn.chen@broadwaytw.onmicrosoft.com>
2021-01-26 15:59:03 +0800

    bug fix

--- Github_Lab/Program.cs
+++ Github_Lab/Program.cs
@@ -15,7 +15,7 @@ namespace Github_Lab
 
             Animal snake = new Snake();
             snake.Move();
-            snake.Move();
+            bird.Move();
 
             Console.WriteLine("Animal World!");
         }
