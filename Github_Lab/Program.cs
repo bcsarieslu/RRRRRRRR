4e45288c4803a4c66e74a31b4c09803b4031f511
Glenn Chen <glenn.chen@broadwaytw.onmicrosoft.com>
2021-01-26 11:58:39 +0800

    加入專案檔案。

---
+++ Github_Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Github_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
